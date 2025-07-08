5d5571d544fa00b6f817b35409491d1aa4aa7667
Extbech <betternerfakali@gmail.com>
2025-07-08 21:23:34 +0200

    yessir

--- src/bin/train.rs
+++ src/bin/train.rs
@@ -6,14 +6,14 @@ use rust_rl::{
     train, Agent,
 };
 
-const GRID_SIZE: (usize, usize) = (3, 3);
+const GRID_SIZE: (usize, usize) = (9, 9);
 
 fn main() {
     let start = Instant::now();
     let mut env = GridEnvironment::new(GRID_SIZE.0, GRID_SIZE.1);
     let mut agent = QAgent::new();
     agent.try_init(&env);
-    train::train(&mut env, &mut agent, 10_000_000);
+    train::train(&mut env, &mut agent, 1_000_000);
     agent
         .save_to_file("data/q_table.json")
         .expect("Failed to save Q-table to file");
