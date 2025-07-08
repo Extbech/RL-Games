ea5e1966e3ac46b1ab842868996971aacd5521d9
Extbech <betternerfakali@gmail.com>
2025-07-08 22:36:18 +0200

    update

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -1,1 +1,2 @@
 pub mod move_to_center;
+pub mod tic_tac_toe;
---
+++ src/environment/tic_tac_toe.rs
@@ -0,0 +1,240 @@
+use crate::{Space, SpaceElem};
+use serde::{Deserialize, Serialize};
+
+use crate::{Action, Environment, Step};
+
+#[derive(Debug, Copy, Clone, Deserialize, Serialize, PartialEq, Eq, Hash, Default)]
+pub struct TicTacAction(usize, usize);
+
+impl SpaceElem for TicTacAction {
+    fn discrete(&self, d: usize) -> Option<usize> {
+        match d {
+            0 => Some(self.0),
+            1 => Some(self.1),
+            _ => None,
+        }
+    }
+
+    fn continuous(&self, _d: usize) -> Option<f32> {
+        None
+    }
+
+    fn try_build(_: &impl Space, discrete: &[usize], continuous: &[f32]) -> Option<Self> {
+        if discrete.len() == 2 && continuous.is_empty() {
+            Some(Self(discrete[0], discrete[1]))
+        } else {
+            None
+        }
+    }
+}
+
+impl Action for TicTacAction {
+    fn is_valid(&self, space: &impl Space) -> bool {
+        space.discrete_dim(0).is_some()
+            && space.discrete_dim(1).is_some()
+            && self.0 < space.discrete_dim(0).unwrap()
+            && self.1 < space.discrete_dim(1).unwrap()
+    }
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub enum CellState {
+    Empty,
+    X,
+    O,
+}
+
+impl CellState {
+    /// Converts a usize to an CellState enum.
+    pub fn from_usize(action: usize) -> Self {
+        match action {
+            0 => CellState::Empty,
+            1 => CellState::X,
+            2 => CellState::O,
+            _ => panic!("Invalid action"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+pub struct Board {
+    pub cells: [[CellState; 3]; 3],
+}
+
+impl SpaceElem for Board {
+    fn discrete(&self, d: usize) -> Option<usize> {
+        match d {
+            0 => Some(self.cells[0][0] as usize),
+            1 => Some(self.cells[0][1] as usize),
+            2 => Some(self.cells[0][2] as usize),
+            3 => Some(self.cells[1][0] as usize),
+            4 => Some(self.cells[1][1] as usize),
+            5 => Some(self.cells[1][2] as usize),
+            6 => Some(self.cells[2][0] as usize),
+            7 => Some(self.cells[2][1] as usize),
+            8 => Some(self.cells[2][2] as usize),
+            _ => None,
+        }
+    }
+
+    fn continuous(&self, _d: usize) -> Option<f32> {
+        None
+    }
+
+    fn try_build(_: &impl Space, discrete: &[usize], continuous: &[f32]) -> Option<Self> {
+        if discrete.len() == 9 && continuous.is_empty() {
+            let mut temp = Self {
+                cells: [[CellState::Empty; 3]; 3],
+            };
+            for n in discrete.iter().enumerate() {
+                let row = n.0 / 3;
+                let col = n.0 % 3;
+                temp.cells[row][col] = CellState::from_usize(*n.1);
+            }
+            Some(temp)
+        } else {
+            None
+        }
+    }
+}
+#[derive(Default, Clone)]
+pub struct Shape;
+
+impl Space for Shape {
+    fn discrete_dim(&self, d: usize) -> Option<usize> {
+        if d < 9 {
+            Some(3)
+        } else {
+            None
+        }
+    }
+
+    fn continuous_dim(&self, _d: usize) -> Option<std::ops::Range<f32>> {
+        None
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct TicTacActionSpace;
+
+impl Space for TicTacActionSpace {
+    fn discrete_dim(&self, d: usize) -> Option<usize> {
+        match d {
+            0 => Some(3), // Rows
+            1 => Some(3), // Columns
+            _ => None,
+        }
+    }
+
+    fn continuous_dim(&self, _d: usize) -> Option<std::ops::Range<f32>> {
+        None
+    }
+}
+pub enum TicTacPlayer {
+    X,
+    O,
+}
+
+pub struct TicTacEnvironment {
+    pub board: Board,
+    pub reward: f32,
+    pub done: bool,
+    pub player: TicTacPlayer,
+}
+
+impl TicTacEnvironment {
+    pub fn new() -> Self {
+        TicTacEnvironment {
+            board: Board {
+                cells: [[CellState::Empty; 3]; 3],
+            },
+            reward: 0.0,
+            done: false,
+            player: TicTacPlayer::X,
+        }
+    }
+
+    fn calc_reward(&mut self) {
+        // Check rows, columns, and diagonals for a win
+        for i in 0..3 {
+            if self.board.cells[i][0] == self.board.cells[i][1]
+                && self.board.cells[i][1] == self.board.cells[i][2]
+            {
+                if self.board.cells[i][0] != CellState::Empty {
+                    self.reward = 1.0; // Win
+                }
+            }
+            if self.board.cells[0][i] == self.board.cells[1][i]
+                && self.board.cells[1][i] == self.board.cells[2][i]
+            {
+                if self.board.cells[0][i] != CellState::Empty {
+                    self.reward = 1.0; // Win
+                }
+            }
+        }
+        if (self.board.cells[0][0] == self.board.cells[1][1]
+            && self.board.cells[1][1] == self.board.cells[2][2])
+            || (self.board.cells[0][2] == self.board.cells[1][1]
+                && self.board.cells[1][1] == self.board.cells[2][0])
+        {
+            if self.board.cells[1][1] != CellState::Empty {
+                self.reward = 1.0; // Win
+            }
+        }
+    }
+}
+
+impl Environment for TicTacEnvironment {
+    type Action = TicTacAction;
+    type State = Board;
+    type StateSpace = Shape;
+    type ActionSpace = TicTacActionSpace;
+
+    fn action_space(&self) -> &Self::ActionSpace {
+        &TicTacActionSpace
+    }
+
+    fn state_space(&self) -> &Self::StateSpace {
+        &Shape
+    }
+
+    /// Resets the environment and sets a new random starting position so that our agent does not always start in the top-left corner.
+    fn reset(&mut self) -> &Self::State {
+        self.reward = 0.0;
+        self.done = false;
+        self.board = Board {
+            cells: [[CellState::Empty; 3]; 3],
+        };
+        &self.board
+    }
+
+    /// Steps through the environment based on the action taken by the agent.
+    /// It updates the agent's position, calculates the reward, and checks if the game is finished.
+    fn step(&mut self, action: &Self::Action) -> Step<Self> {
+        match self.player {
+            TicTacPlayer::X => {
+                if self.board.cells[action.0][action.1] == CellState::Empty {
+                    self.board.cells[action.0][action.1] = CellState::X;
+                    self.calc_reward();
+                    self.player = TicTacPlayer::O;
+                } else {
+                    self.reward = -1.0; // Invalid move
+                }
+            }
+            TicTacPlayer::O => {
+                if self.board.cells[action.0][action.1] == CellState::Empty {
+                    self.board.cells[action.0][action.1] = CellState::O;
+                    self.calc_reward();
+                    self.player = TicTacPlayer::X;
+                } else {
+                    self.reward = -1.0; // Invalid move
+                }
+            }
+        }
+        Step {
+            is_final: self.done,
+            reward: self.reward,
+            next_state: &self.board,
+        }
+    }
+}
