f9fa0146d3cc544d1b440b86912fa39208d85943
Extbech <betternerfakali@gmail.com>
2025-07-12 02:44:43 +0200

    it almost compiles

--- src/agents/dqn_agent.rs
+++ src/agents/dqn_agent.rs
@@ -1,7 +1,13 @@
-use std::ops::Range;
-use serde::{Serialize, Deserialize};
+use crate::{
+    agents::{
+        network::nn::{ActivationFunction, LossFunction, NeuralNetwork},
+        q_agent::{all_actions, ALPHA_DEFAULT, EPSILON_DEFAULT, GAMMA_DEFAULT},
+    },
+    Action, Agent, Environment, Space, SpaceElem,
+};
 use rand::prelude::*;
-use crate::{agents::{network::nn::{ActivationFunction, LossFunction, NeuralNetwork}, q_agent::{all_actions, ALPHA_DEFAULT, EPSILON_DEFAULT, GAMMA_DEFAULT}}, Action, Agent, Environment, Space, SpaceElem};
+use serde::{Deserialize, Serialize};
+use std::ops::Range;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct DQNAgent {
@@ -20,7 +26,7 @@ impl DQNAgent {
                 ALPHA_DEFAULT as f64,
                 ActivationFunction::Sigmoid,
                 ActivationFunction::Sigmoid,
-                LossFunction::MeanSquaredError
+                LossFunction::MeanSquaredError,
             ),
             epsilon: EPSILON_DEFAULT,
             gamma: GAMMA_DEFAULT,
@@ -30,7 +36,7 @@ impl DQNAgent {
         }
     }
 
-    fn encode_input(&self, state: &impl SpaceElem, action: &impl Action) -> Vec<f64> {
+    fn encode_input(&self, state: &impl SpaceElem) -> Vec<f64> {
         let mut input = vec![];
         for d in 0..self.disc_state_space.len() {
             input.push(state.discrete(d).unwrap() as f64 / self.disc_state_space[d] as f64);
@@ -40,15 +46,12 @@ impl DQNAgent {
             let range = self.cont_state_space[d].end - self.cont_state_space[d].start;
             input.push((state.continuous(d).unwrap() as f64 - offset) / range as f64);
         }
-        for d in 0..self.action_space.len() {
-            input.push(action.discrete(d).unwrap() as f64 / self.action_space[d] as f64);
-        }
         input
     }
 
-    fn q_val(&self, state: &impl SpaceElem, action: &impl Action) -> f64 {
-        let input = self.encode_input(state, action);
-        self.network.predict(input)[0]
+    fn predict_network(&self, state: &impl SpaceElem) -> Vec<f64> {
+        let input = self.encode_input(state);
+        self.network.predict(input)
     }
 
     pub fn load_from_file(file_path: &str) -> Result<Self, String> {
@@ -71,11 +74,11 @@ impl<E: Environment> Agent<E> for DQNAgent {
             return false; // DQN does not support continuous action spaces
         }
         (self.disc_state_space, self.cont_state_space) = env.state_space().as_vecs();
-        let tot_dims = self.disc_state_space.len() + self.cont_state_space.len() +
-            self.action_space.len();
+        let input_dims = self.disc_state_space.len() + self.cont_state_space.len();
+        let output_dims = self.action_space.iter().product();
         // First layer has size of total dimensions of state and action spaces
         // Last Layer has only one ouput, the Q-value
-        self.network.add_layers(&[tot_dims, 64, 1]);
+        self.network.add_layers(&[input_dims, 64, output_dims]);
         true
     }
 
@@ -90,17 +93,17 @@ impl<E: Environment> Agent<E> for DQNAgent {
     }
 
     fn learn(
-            &mut self,
-            old_state: &<E as Environment>::State,
-            action: &<E as Environment>::Action,
-            reward: f32,
-            next_state: Option<&<E as Environment>::State>,
-        ) {
-        let input = self.encode_input(old_state, action);
+        &mut self,
+        old_state: &<E as Environment>::State,
+        action: &<E as Environment>::Action,
+        reward: f32,
+        next_state: Option<&<E as Environment>::State>,
+    ) {
+        let input = self.encode_input(old_state);
         let mut max_q_next = f64::MIN;
         if let Some(next_state) = next_state {
             for a in all_actions::<E::Action>(&self.action_space) {
-                let q_val = self.q_val(next_state, &a);
+                let q_val = self.predict_network(next_state);
                 if q_val > max_q_next {
                     max_q_next = q_val;
                 }
@@ -109,20 +112,21 @@ impl<E: Environment> Agent<E> for DQNAgent {
             // If there is no next state it is terminal, so we set max_q_next to 0
             max_q_next = 0.0;
         }
-        self.network.train(vec![input], vec![vec![self.gamma as f64 * max_q_next + reward as f64]]);
+        self.network.train(
+            vec![input],
+            vec![vec![self.gamma as f64 * max_q_next + reward as f64]],
+        );
     }
 
     fn predict(&self, state: &<E as Environment>::State) -> <E as Environment>::Action {
         // Exploitation: choose the best action based on Q-values
-        let mut best_action = E::Action::default();
-        let mut best_value = f32::MIN;
-        for action in all_actions::<E::Action>(&self.action_space) {
-            let q_value: f32 = self.q_val(state, &action) as f32;
-            if q_value > best_value {
-                best_value = q_value;
-                best_action = action;
-            }
-        }
-        best_action
+        let action = self
+            .predict_network(state)
+            .iter()
+            .enumerate()
+            .max_by(|(_i, x), (_j, y)| x.partial_cmp(y).unwrap())
+            .unwrap()
+            .0;
+        all_actions(&self.action_space).take(action).next().unwrap()
     }
 }
